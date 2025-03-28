6fcf02dbcee1860c3f43d14bcdaeed783a23a7ba
nirmalraj1610 <nirmal16ags@gmail.com>
2025-03-28 17:54:32 +0530

    init

---
+++ db.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+
+const connectToMongoDB = async () => {
+    console.log(process.env.url)
+    try {
+        await mongoose.connect("mongodb+srv://navinvenkatv:navinvenkatv@cluster0.sidyjsl.mongodb.net/");
+        console.log("Connected to MongoDB successfully");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); 
+    }
+};
+
+connectToMongoDB();
+
+const userSchema = new mongoose.Schema({
+    carName: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+const Cars = mongoose.model("Car", userSchema);
+
+module.exports = { Cars };
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express")
+const cors  = require("cors")
+const {Cars} = require("./db")
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.post("/create", async(req, res)=>{
+    const { carName, price } = req.body;
+    try{
+       const user = await Cars.create({
+        carName, 
+        price
+       })
+       console.log("Details Submitted!")
+       return res.json({
+        id : user.id
+       })
+    }catch(e){
+        console.log(e)
+        res.json({
+            msg : "Something went wrong!"
+        })
+        return;
+    }
+})
+
+app.delete('/delete', async (req, res)=>{
+    console.log("inside delete route")
+    const { id } = req.query;
+    try{
+        const user = await Cars.findByIdAndDelete(id);
+        res.json({
+            msg : "Deleted Successfully"
+        })
+        return;
+    }catch(e){
+        console.log(e)
+        res.json({
+            msg : "Something went wrong!"
+        })
+        return;
+    }
+})
+
+app.get('/all', async (req, res)=>{
+    try{
+        const user = await Cars.find();
+        res.json({
+            user
+        })
+        return;
+    }catch(e){
+        console.log(e)
+        res.json({
+            msg : "Something went wrong!"
+        })
+        return;
+    }
+})
+
+app.put('/update', async (req, res)=>{
+    try{
+        const { id } = req.query;
+        const body = req.body;
+        const user = await Cars.findByIdAndUpdate(id,{
+            carName : body.carName,
+            price   : body.price
+        })
+        return res.json({
+            id : user.id
+        })
+    }catch(e){
+        console.log(e)
+        res.json({
+            msg : "Something went wrong!"
+        })
+        return;
+    }
+})
+
+app.listen(3000,()=>{
+    console.log("Running on port ", 3000)
+})
